aac02f297476df405b954ad6d80f0d1ad334b151
cmr927 <142535038+cmr927@users.noreply.github.com>
2024-01-18 15:12:34 -0500

    Updated connection URI

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ require('./passport');
 // mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Online connection
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //READ
 //default text response when at /
